7b38cb364714592ec8c3affbb0331818d0c17156
Julia <provotorova.97@mail.ru>
2021-11-23 22:02:40 +0300

    rework

--- task_4/solution.js
+++ task_4/solution.js
@@ -3,8 +3,7 @@
 function parseProducts(json) {
     // Аргументом функции является JSON
     let data = JSON.parse(json)
-    let = products 
-    products = data.products
+    let products = data.products
     return products
     // Преобразуйте строку json, переданную как аргумент функции,
     // в объект с помощью функции JSON.parse(json)
@@ -16,8 +15,9 @@ function renderProductsCards(json) {
     clearProducts()
     products = parseProducts(json)
     length = products.length
-    for (let i = 0; i < length; i += 1)
+    for (let i = 0; i < length; i += 1) {
     addProduct(products[i]);
+  }
 }
 // Напишите функцию renderProductsCards(json)
 // Аргументом функции является JSON
